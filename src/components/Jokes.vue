b19493a3df6e49b244c598c13f8c9b9ebb1630f3
Alexandros Seme <alexandros.seme@gmail.com>
2025-03-23 22:01:26 +0200

    Add favorites table / create add-delete interaction

--- src/components/Jokes.vue
+++ src/components/Jokes.vue
@@ -1,79 +1,141 @@
 <template>
-    <div class="w-full max-w-3xl bg-white mx-auto p-6 shadow-md rounded-md">
-      <div class="flex justify-center gap-4 mb-6">
-        <button id="randomButton" @click="fetchJoke('random')" :class="buttonClass('random')">
-          <font-awesome-icon icon="check" class="mr-2" />
-          Random
-        </button>
-        <button id="programmingButton" @click="fetchJoke('programming')" :class="buttonClass('programming')">
-          <font-awesome-icon icon="check" class="mr-2" />
-          Programming
-        </button>
-      </div>
-      <div v-if="joke" class="bg-gray-50 p-5 rounded-lg shadow-inner">
-        <h2 class="text-lg font-semibold mb-2 capitalize text-gray-700">
-          {{ joke.type }} Joke
-        </h2>
-        <p class="text-gray-600 mb-2">
-          <span class="mr-2">
-            <font-awesome-icon icon="circle-exclamation" class="text-blue-400 info mr-2" />
-          </span>{{ joke.setup }}
-        </p>
-        <p class="font-semibold text-gray-800">
-          <span class="mr-2">
-            <font-awesome-icon icon="face-smile" class="text-gray-500 mr-2" />
-          </span>{{ joke.punchline }}
-        </p>
-      </div>
+    <div class="w-full max-w-3xl bg-white mx-auto p-6 shadow-md rounded-md relative">
+        <font-awesome-icon @click="addJoke(joke)" icon="heart-circle-plus"
+            class="absolute top-4 right-4 text-red-500 text-2xl cursor-pointer z-10" />
+        <div class="flex justify-center gap-4 mb-6">
+            <button id="randomButton" @click="fetchJoke('random')" :class="buttonClass('random')">
+                <font-awesome-icon icon="check" class="mr-2" />
+                Random
+            </button>
+            <button id="programmingButton" @click="fetchJoke('programming')" :class="buttonClass('programming')">
+                <font-awesome-icon icon="check" class="mr-2" />
+                Programming
+            </button>
+        </div>
+        <div v-if="joke" class="bg-gray-50 p-5 rounded-lg shadow-inner">
+            <h2 class="text-lg font-semibold mb-2 capitalize text-gray-700">
+                {{ joke.type }} Joke
+            </h2>
+            <p class="text-gray-600 mb-2">
+                <span class="mr-2">
+                    <font-awesome-icon icon="circle-exclamation" class="text-blue-400 info mr-2" />
+                </span>{{ joke.setup }}
+            </p>
+            <p class="font-semibold text-gray-800">
+                <span class="mr-2">
+                    <font-awesome-icon icon="face-smile" class="text-gray-500 mr-2" />
+                </span>{{ joke.punchline }}
+            </p>
+        </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  import { getProgrammingJokes, getRandomJoke } from '../services/requests'
-  
-  type JokeType = 'random' | 'programming' | null
-  const selected = ref<JokeType>(null)
-  const joke = ref<any | null>(null)
-  
-  async function fetchJoke(type: JokeType) {
-    selected.value = type  
+    <div class="mt-10 overflow-x-auto w-full max-w-3xl bg-white mx-auto p-6 shadow-md rounded-md relative">
+        <h1 class="text-3xl font-bold mb-5 text-gray-500 text-center w-full">Favorite Jokes</h1>
+        <table class="w-full table-auto border border-gray-300 rounded-md shadow-sm">
+            <thead class="bg-gray-100">
+                <tr>
+                    <th class="px-2 py-2 text-left text-gray-500">Type</th>
+                    <th class="px-2 py-2 text-left text-gray-500">Setup</th>
+                    <th class="px-2 py-2 text-left text-gray-500">Punchline</th>
+                    <th class="px-2 py-2 text-center text-gray-500">Rating</th>
+                    <th class="px-2 py-2 text-center text-gray-500">Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(joke, index) in tableJokes" class="border-t hover:bg-gray-50 transition">
+                    <td class="px-2 py-2 capitalize">{{ joke.type }}</td>
+                    <td class="px-2 py-2">{{ joke.setup }}</td>
+                    <td class="px-2 py-2">{{ joke.punchline }}</td>
+                    <td class="px-2 py-2 text-center stars">
+                        <font-awesome-icon v-for="star in 5" :icon="joke.rating >= star ? 'star' : 'star-half-alt'"
+                            class="cursor-pointer text-yellow-500 mx-0.5" @click="setRating(joke, star)" />
+                    </td>
+                    <td class="px-2 py-2 text-center">
+                        <button class="text-red-600 hover:text-red-800 transition  cursor-pointer"
+                            @click="removeJoke(joke.id)">
+                            <font-awesome-icon icon="trash" />
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { getProgrammingJokes, getRandomJoke } from '../services/requests'
+
+type JokeType = 'random' | 'programming' | null
+const selected = ref<JokeType>(null)
+const joke = ref<any | null>(null)
+const tableJokes = ref<any[]>([]);
+type RatedJoke = { id: number; type: string; setup: string; punchline: string; rating?: number }
+
+async function fetchJoke(type: JokeType) {
+    selected.value = type
     try {
-      if (type === 'random') {
-        joke.value = await getRandomJoke()
-      } else if (type === 'programming') {
-        joke.value = await getProgrammingJokes()
-      }
-      console.log('Joke fetched:', joke.value)
+        if (type === 'random') {
+            joke.value = await getRandomJoke()
+        } else if (type === 'programming') {
+            joke.value = await getProgrammingJokes()
+        }
+
+        console.log('Joke fetched:', joke.value)
     } catch (error) {
-      console.error('Error fetching joke:', error)
+        console.error('Error fetching joke:', error)
     }
-  }
-  
-  function buttonClass(type: JokeType) {
+}
+
+function buttonClass(type: JokeType) {
     const base = 'px-4 py-2 rounded-full border font-medium transition-all flex items-center cursor-pointer'
-    const isActive = selected.value === type  
+    const isActive = selected.value === type
     return isActive
-      ? `${base} button-active text-white border-transparent`
-      : `${base} bg-white text-gray-700 border-gray-300 hover:bg-gray-200`
-  }
-  </script>
-  
-  <style scoped>
-  .button-active {
+        ? `${base} button-active text-white border-transparent`
+        : `${base} bg-white text-gray-700 border-gray-300 hover:bg-gray-200`
+}
+
+function addJoke(joke: RatedJoke) {
+    const exists = tableJokes.value.find(existing => existing.id === joke.id)
+    if (!exists && joke != null) {
+        tableJokes.value.push({ ...joke, rating: 0 })
+    } else {
+        console.log('Already exist')
+    }
+}
+
+
+function setRating(joke: RatedJoke, rating: number) {
+    const index = tableJokes.value.findIndex(el => el.id === joke.id)
+    if (index !== -1) {
+        tableJokes.value[index].rating = rating
+    }
+}
+function removeJoke(id: number) {
+    tableJokes.value = tableJokes.value.filter(joke => joke.id !== id)
+}
+
+</script>
+
+<style scoped>
+.button-active {
     background-color: #5d648d;
-  }
-  .button-active:hover {
+}
+
+.button-active:hover {
     background-color: #4b4f6d;
-  }
-  
-  #randomButton:hover,
-  #programmingButton:hover {
+}
+
+#randomButton:hover,
+#programmingButton:hover {
     cursor: pointer;
-  }
-  
-  .info {
+}
+
+.info {
     color: rgb(45, 104, 240);
-  }
-  </style>
-  
+}
+
+.stars {
+    width: 20%;
+}
+</style>
--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHeart, faTrash, faStar, faInfoCircle, faCheck , faHeartCirclePlus , faExclamationCircle, faFaceSmile} from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faTrash, faStar, faInfoCircle, faCheck , faHeartCirclePlus , faExclamationCircle, faFaceSmile, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faHeart, faTrash, faStar, faInfoCircle, faCheck, faHeartCirclePlus, faExclamationCircle, faFaceSmile)
+library.add(faHeart, faTrash, faStar, faInfoCircle, faCheck, faHeartCirclePlus, faExclamationCircle, faFaceSmile, faStarHalfAlt)
 
 export default FontAwesomeIcon
