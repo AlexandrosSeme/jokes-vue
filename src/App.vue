1a5746441269f79c7b753e972bd83fcad617d859
Alexandros Seme <alexandros.seme@gmail.com>
2025-03-23 14:23:55 +0200

    Delete default pages and add joke page / also config tsconfig.json for typescript

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Jokes from './components/Jokes.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      You did it!
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main> -->
+  <Jokes />
+  <!-- </main> -->
 </template>
 
 <style scoped>
---
+++ src/components/Jokes.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="joke text-center text-xl text-green-600">
+        Jokes works!
+    </div>
+</template>
+
+<script setup lang="ts">
+// Simple Jokes component
+</script>
+
+<style scoped>
+.joke {
+    margin-top: 0.2rem;
+    display: flex;
+    position: relative;
+}
+</style>
